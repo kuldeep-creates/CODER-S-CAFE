cccda88cb41a74c685b8f0e72d66e8781dcfe0df
KULDEEP-CREATES <kuldeepjangid2008@gmail.com>
2025-07-21 10:07:43 +0530

    Update site with latest build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/CODER-S-CAFE/', 
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
