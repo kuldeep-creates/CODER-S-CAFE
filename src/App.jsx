556fa6a912d4fa132256c898d5db2f2b8076c1d8
KULDEEP-CREATES <kuldeepjangid2008@gmail.com>
2025-07-21 10:17:41 +0530

    Update site with latest build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Hero from './components/Hero.jsx';
 import Domains from './components/Domains.jsx';
